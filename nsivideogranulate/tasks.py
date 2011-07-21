a69ebf4f4598c005685a07f0a4c22a7c7507e73f
Douglas Camata <d.camata@gmail.com>
2011-07-21 17:07:11 +0000

    recebendo configurações para o SAM do webserver

--- nsivideogranulate/tasks.py
+++ nsivideogranulate/tasks.py
@@ -9,14 +9,14 @@ class VideoException(Exception):
     pass
 
 @task
-def granulate_video(grains_uid, video_uid, callback_url):
+def granulate_video(grains_uid, video_uid, callback_url, sam_settings):
     print "Starting new job."
-    grains = get_from_sam(grains_uid)
+    grains = get_from_sam(grains_uid, sam_settings)
     grains = grains.resource()
-    video = get_from_sam(video_uid).resource()
+    video = get_from_sam(video_uid, sam_settings).resource()
     if hasattr(grains.data, 'done') and not grains.data.done:
         print "Starting the granularization..."
-        granulate(video_uid, grains_uid)
+        granulate(video_uid, grains_uid, sam_settings)
         print "Done the granularization."
         if not callback_url == None:
             print callback_url
@@ -28,17 +28,18 @@ def granulate_video(grains_uid, video_uid, callback_url):
     else:
         raise VideoException("Video already granulated.")
 
-def granulate(video_uid, grains_uid):
-    video = get_from_sam(video_uid).resource().data
+def granulate(video_uid, grains_uid, sam_settings):
+    video = get_from_sam(video_uid, sam_settings).resource().data
     granulate = Granulate()
     grains = granulate.granulate('nothing.ogv', decodestring(video))
     encoded_grains = [b64encode(image.getContent().getvalue()) for image in grains['image_list']]
-    store_in_sam(grains_uid, {'grains':encoded_grains})
+    store_in_sam(grains_uid, {'grains':encoded_grains}, sam_settings)
 
-def store_in_sam(uid, video):
-    sam = Restfulie.at("http://0.0.0.0:8888/").as_("application/json").auth('test', 'test')
+def store_in_sam(uid, video, sam_settings):
+    sam = Restfulie.at(sam_settings['url']).as_('application/json').auth(*sam_settings['auth'])
     return sam.post(key=uid, value=video)
 
-def get_from_sam(uid):
-    sam = Restfulie.at("http://0.0.0.0:8888/").as_("application/json").auth('test', 'test')
+def get_from_sam(uid, sam_settings):
+    sam = Restfulie.at(sam_settings['url']).as_('application/json').auth(*sam_settings['auth'])
     return sam.get(key=uid)
+
